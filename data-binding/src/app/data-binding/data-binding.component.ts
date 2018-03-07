0e72ae5bc8356d6d2258a156f6551e4c79a219f8
Danilo Pereira <danilo4web@gmail.com>
2018-03-07 11:45:19 -0300

    Two Way Data Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  // styleUrls: ['./data-binding.component.css']
+
+  styles: [ ` .highlight { background: yellow; } `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http://cursos.com.br';
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/sports/';
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  valorBlur: string = '';
+
+  isMouseOver = false;
+
+  nome: string = 'abc';
+
+  pessoa: any = {
+    nome: 'def',
+    idade: 20
+  }
 
   getValor() {
     return 5;
@@ -23,8 +39,30 @@ export class DataBindingComponent implements OnInit {
     alert('botao clicado!');
   }
 
-  onKeyUp(evento) {
-    console.log(evento);
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  salvarBlur(valor) {
+    this.valorBlur = valor;
+  }
+
+  salvarClicado(valor) {
+    this.valorClicado = valor;
+  }
+
+  onmouseover() {
+    this.isMouseOver = true;
+    console.log('passou');
+  }
+
+  onmouseout() {
+    this.isMouseOver = false;
+    console.log('saiu');
   }
 
   constructor() { }
