a265bfc319f126a16e7ac59b9646687d75566508
Danilo <danilo4web@gmail.com>
2018-06-11 23:22:45 -0300

    implemented detail for restaurants

--- udemy-course/meat-app-starter/src/app/app.routes.ts
+++ udemy-course/meat-app-starter/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from "@angular/router";
 
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { AboutComponent } from './about/about.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'restaurants', component: RestaurantsComponent},
-    // {path: 'restaurant/:id', component: RestauratComponent},
+    {path: 'restaurant/:id', component: RestaurantDetailComponent},    
     {path: 'about', component: AboutComponent},
     
 ]
